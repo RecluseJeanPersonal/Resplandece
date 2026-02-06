09e357d41fb68b2efed5c754e89d29728c90dd7d
RecluseJean <82900429+RecluseJean@users.noreply.github.com>
2026-02-06 14:52:38 -0500

    cors

--- src/main/java/app_iglesia/security/SecurityConfig.java
+++ src/main/java/app_iglesia/security/SecurityConfig.java
@@ -69,7 +69,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "https://resplance2025.web.app/"
+                "https://resplance2025.web.app/",
+                "https://aromademujer-8b144.web.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos HTTP permitidos
         config.setAllowedHeaders(List.of("*")); // Permite todos los encabezados
